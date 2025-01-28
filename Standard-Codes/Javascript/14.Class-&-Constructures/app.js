2038e9de872fb55aad110f950a2e9081508ee2bd
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-01-28 17:22:02 +0530

    Learned classes and constructures in javascript

---
+++ Standard-Codes/Javascript/14.Class-&-Constructures/app.js
@@ -0,0 +1,32 @@
+//--------------------------------------------------------------------------------------
+// // constructor
+// function Person(firstName, lastName, age) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+//   this.intro = function () {
+//     console.log(`\n${firstName} ${lastName} is ${age} years old.\n`);
+//   };
+// }
+
+// const i = new Person("i", "am", 20);
+// i.intro();
+// const me = new Person("me", "myself", 100);
+// me.intro();
+
+//--------------------------------------------------------------------------------------
+
+// class person {
+//     constructor(firstName, lastName, age){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.age = age;
+//         this.intro = function () {
+//             return `${firstName} ${lastName} is ${age} years old`;  
+//         };
+//     }
+// }
+
+// const i = new person("I", "Am", 25);
+
+//--------------------------------------------------------------------------------------
