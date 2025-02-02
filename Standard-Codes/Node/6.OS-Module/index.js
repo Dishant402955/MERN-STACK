772db49d0709b3477333a3e231cb8f6034847924
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-02-02 19:58:07 +0530

    Learned basics of URL & OS Module

---
+++ Standard-Codes/Node/6.OS-Module/index.js
@@ -0,0 +1,9 @@
+import * as os from "os";
+
+// console.log(os.platform());
+// console.log(os.arch());
+// console.log(os.cpus());
+// console.log(os.hostname());
+// console.log(os.networkInterfaces());
+// console.log(`Free Memory in bytes ${os.freemem()}`);
+// console.log(`Total Memory in bytes ${os.totalmem()}`);
---
+++ Standard-Codes/Node/7.URL-Module/index.js
@@ -0,0 +1,18 @@
+import * as url from "url";
+
+const myURL = new url.URL("https://www.example.fake:8080/p/a/t/h?query=string#value");
+
+// console.log();
+// console.log(myURL);
+// console.log(myURL.hash);
+// console.log(myURL.host);
+// console.log(myURL.origin);
+// console.log(myURL.protocol);
+// console.log(myURL.port);
+// console.log(myURL.pathname);
+// console.log(myURL.password);
+// console.log(myURL.username);
+// console.log(myURL.search);
+// console.log(myURL.searchParams);
+// console.log(myURL.href);
+// console.log();
